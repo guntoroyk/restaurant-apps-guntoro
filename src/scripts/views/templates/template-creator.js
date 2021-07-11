58db6b8d5e7cfe08ac3559de1ca99dbce84e7330
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2021-07-11 13:14:38 +0700

    fix icon not shown

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -45,13 +45,13 @@ const createRestaurantReviewItemTemplate = (review) => `
 
 const createLikeButtonTemplate = () => `
   <button aria-label="like this movie" id="likeButton" class="like">
-     <i class="fa fa-heart-o" aria-hidden="true"></i>
+     <i class="far fa-heart" aria-hidden="true"></i>
   </button>
 `;
 
 const createLikedButtonTemplate = () => `
   <button aria-label="unlike this movie" id="likeButton" class="like">
-    <i class="fa fa-heart" aria-hidden="true"></i>
+    <i class="fas fa-heart" aria-hidden="true"></i>
   </button>
 `;
 
